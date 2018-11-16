1ce1c497707e94f7401fa8ddfbdd69ec86ebfd74
AntaeusNar <28239049+AntaeusNar@users.noreply.github.com>
2018-11-16 12:34:00 -0800

    added the Topic model to models.py

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Topic(models.Model):
+    """A topic the user is learning about"""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __string__(self):
+        """return a string definition of self"""
+        return self.text
