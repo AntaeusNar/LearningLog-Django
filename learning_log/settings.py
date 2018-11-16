ca78f25cc014a816a4fbf6a7a9617c18da1d7256
AntaeusNar <28239049+AntaeusNar@users.noreply.github.com>
2018-11-16 12:41:44 -0800

    Updated setting.py in the Django framework to look for my created app

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # My created apps
+    'learning_logs',
 ]
 
 MIDDLEWARE = [
