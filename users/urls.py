e1b25c5db67da57c5ca19e494d6a3f6ace3d9139
AntaeusNar <28239049+AntaeusNar@users.noreply.github.com>
2018-11-16 20:44:40 -0800

    setup registering new users

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,18 @@
 """Defines url paths for the users section"""
 
 from django.urls import path
-from django.contrib.auth.views import login
+from django.contrib.auth import views as auth_views
 
 from . import views
 
 app_name = 'users'
 urlpatterns = [
     # Login page
-    path('login/', login, {'template_name': 'users/login.html'}, name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+
+    # Logout page.
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
+    # Registration page
+    path('register/', views.register, name='register')
 ]
