ca33362d76a0703474ae8f039d88da9c639987ca
AntaeusNar <28239049+AntaeusNar@users.noreply.github.com>
2018-11-16 19:10:41 -0800

    created urls.py for users app

---
+++ users/urls.py
@@ -0,0 +1,12 @@
+"""Defines url paths for the users section"""
+
+from django.urls import path
+from django.contrib.auth.views import login
+
+from . import views
+
+app_name = 'users'
+urlpatterns = [
+    # Login page
+    path('login/', login, {'template_name': 'users/login.html'}, name='login'),
+]
