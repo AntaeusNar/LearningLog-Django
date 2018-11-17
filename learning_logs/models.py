1cb68143e866272a5d47291682f50caf2f7f5f39
AntaeusNar <28239049+AntaeusNar@users.noreply.github.com>
2018-11-16 20:58:26 -0800

    added user ownership to topics

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
@@ -8,6 +9,7 @@ class Topic(models.Model):
     """A topic the user is learning about"""
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         """return a string definition of self"""
@@ -26,3 +28,5 @@ class Entry(models.Model):
     def __str__(self):
         """returns the string."""
         return self.text[:50] + "..."
+
+
